9db266e47f18d0ce9d6f6ecc33f92632aa23e00b
mojila <mojila@gitlab.com>
2019-03-22 16:01:19 +0700

    init

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,10 @@
 import App, { Container } from "next/app";
 import Head from "next/head";
 import styled, { createGlobalStyle } from "styled-components";
-import Nav from "../components/Nav";
 import { title } from "./_document";
 
 // Any global CSS variables and selectors we want
 const GlobalStyle = createGlobalStyle`
-  :root {
-    --padding: 2rem;
-    --max-width: 50rem;
-  }
-
   body {
     background: var(--background--1);
     font-family: 'PT Sans', sans-serif;
@@ -44,7 +38,6 @@ export default class MyApp extends App {
           <title>{title}</title>
         </Head>
         <Container>
-          <Nav />
           <Main>
             <Component {...pageProps} router={router} />
           </Main>
--- pages/_document.js
+++ pages/_document.js
@@ -27,7 +27,7 @@ export default class MyDocument extends Document {
           {this.props.styleTags}
 
           {/* Progressive Web App: Match the width of app’s content with width of viewport for mobile devices */}
-          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no" />
 
           {/* Progressive Web App: Have address bar match brand colors */}
           <meta name="theme-color" content="#fff" />
